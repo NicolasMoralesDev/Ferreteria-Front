800683692f90a93b05959f264b92828282e635b1
Nicolas Morales <nicolasmoralesj03@gmail.com>
2024-01-04 18:31:06 -0300

    first commit

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,22 @@
+import ProductList from '../../components/ProductList/ProductList';
+import { Helmet } from 'react-helmet';
+import ProductsCarousel from '../../components/ProductsCarousel';
+
+export default function Home() {
+  return (
+    <>
+      <Helmet>
+        <meta charSet='utf-8' />
+        <title>TECNO TIENDA | HOME</title>
+        <link rel='canonical' href='http://mysite.com/example' />
+        <meta name="description" content="Bienvenido a TecnoTienda! Una web con los mejores precios en Hardware del mercado." />
+      </Helmet>
+
+        <main>
+          <ProductsCarousel/>
+          <ProductList />
+        </main>
+
+    </>
+  );
+}
