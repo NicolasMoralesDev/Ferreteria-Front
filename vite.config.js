ef984eabdac71ff14c353dcb9cafe16ff0c38bf6
Nicolas Morales <nicolasmoralesj03@gmail.com>
2024-03-05 10:47:16 -0300

    se quita dependecia que no se usa

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,9 @@
 import { defineConfig } from 'vite'
-import purgeCSSPlugin from '@fullhuman/postcss-purgecss'
-import { resolvePath } from 'react-router-dom';
 import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-    purgeCSSPlugin({
-      // Specify the paths to your CSS and/or JS files:
-      content: [
-        resolvePath(__dirname, 'index.html'),
-        resolvePath(__dirname, 'src/**/*.{jsx,tsx}'), // Include React components
-      ],
-      // Adjust paths and options as needed (refer to PurgeCSS documentation):
-      safelist: [], // Optionally list classNames that should always be included
-      // Other options as required: https://purgecss.com/configuration/options/
-    }),
+    react()
   ],
   server: {
     watch: {
