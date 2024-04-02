832a65ca759f0579da49adcbaff6972cd9f14c02
Nicolas Morales <nicolasmoralesj03@gmail.com>
2024-04-02 02:11:42 -0300

    se solucina errores responsivos

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,24 +1,25 @@
 import "./footer.css"
 const Footer = () => {
 
-  const data = import.meta.env.VITE_PORT;
 
   return (
     <footer className="footer text-light back position-relative bottom-0 p-4" style={styles.footer}>
       <div className="container-fluid">
         <div className="row cntainer-fluid d-flex flex-row justify-content-center">
-          <div className="col-lg-6">
+
+          <div className="col-lg-4">
             <div className="d-flex justify-content-center flex-column align-items-center">
-              <h3>Links</h3>
-              <ul className="fs-5 text-center">
+              <h3 className="text-center">Links</h3>
+              <ul>
                 <li><a className="link-light" href="/">Home</a></li>
+                <li><a className="link-light" href="/login">Ingresar</a></li>
                 <li><a className="link-light" href="/ayuda">Ayuda</a></li>
               </ul>
             </div>
           </div>
-          <div className="col-lg-6">
+          <div className="col-lg-4">
             <div className="d-flex justify-content-center flex-column align-items-center">
-              <h3>Contacto</h3>
+              <h3 className="text-center">Contacto</h3>
               <ul>
                 <ul>Email:
                   <li>infocorralon@corralononline.com</li>
@@ -27,16 +28,22 @@ const Footer = () => {
                   <li>+54 9 2364 38-0471</li>
                   <li>+54 9 236 451-9612</li>
                 </ul>
-                <ul>Instagram: <li> @corralontelollevo
-                </li></ul>
+              </ul>
+            </div>
+          </div>
+          <div className="col-lg-4">
+            <div className="d-flex justify-content-center flex-column align-items-center">
+              <h3 className="text-center">Nuestras Redes</h3>
+
+                <ul>Instagram:
+                <li> @corralontelollevo</li>
               </ul>
             </div>
           </div>
         </div>
         <div className="text-center m-2">
-          <p className="fs-6">© Copyright 2024 - Desarrollado por Luno y Neuro Marketing</p>
+          <p className="fs-6 text-wrap">© Copyright 2024 - Desarrollado por Luno y Neuro Marketing</p>
         </div>
-
       </div>
     </footer>
   )
@@ -46,6 +53,7 @@ export default Footer
 
 const styles = {
   footer: {
+    textWrap: "balance",
     marginTop: 'auto',
     width: '100%',
   }
